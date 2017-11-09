2a0124cc9c8612019c46b8c3bdada38b0703df49
Marc Geh <marc@geh-engineering.de>
2017-11-09 15:22:00 +0100

    initial commit

---
+++ ControllerOptions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace XOutput
+{
+    public partial class ControllerOptions : Form
+    {
+        ControllerDevice dev;
+        private Thread detectThread;
+        public ControllerOptions(ControllerDevice device)
+        {
+            InitializeComponent();
+            dev = device;
+            int ind = 0;
+
+            foreach (MultiLevelComboBox m in this.Controls.OfType<MultiLevelComboBox>()) {
+                //Tag structure: [Type, Number, Index]
+                m.Items[0] = getBindingText(ind); //Change combobox text according to saved binding
+                m.addOption("Disabled",
+                    tag: new byte[] { 255, 0, (byte)ind });
+                m.addOption("Detect",
+                    tag: new byte[] { 254, 0, (byte)ind });
+                ToolStripMenuItem axes = m.addMenu("Axes");
+                ToolStripMenuItem buttons = m.addMenu("Buttons");
+                ToolStripMenuItem dpads = m.addMenu("D-Pads");
+                ToolStripMenuItem iaxes = m.addMenu("Inverted Axes", axes);
+                ToolStripMenuItem haxes = m.addMenu("Half Axes", axes);
+                ToolStripMenuItem ihaxes = m.addMenu("Inverted Half Axes", axes);
+                for (int i = 1; i <= dev.joystick.Capabilities.ButtonCount; i++)
+                {
+                    m.addOption("Button " + i.ToString(), buttons,
+                        new byte[] { 0, (byte)(i - 1), (byte)ind });
+                }
+                for (int i = 1; i <= dev.joystick.Capabilities.PovCount; i++)
+                {
+                    m.addOption("D-Pad " + i.ToString() + " Up", dpads,
+                        new byte[] { 32, (byte)(i - 1), (byte)ind });
+                    m.addOption("D-Pad " + i.ToString() + " Down", dpads,
+                        new byte[] { 33, (byte)(i - 1), (byte)ind });
+                    m.addOption("D-Pad " + i.ToString() + " Left", dpads,
+                        new byte[] { 34, (byte)(i - 1), (byte)ind });
+                    m.addOption("D-Pad " + i.ToString() + " Right", dpads,
+                        new byte[] { 35, (byte)(i - 1), (byte)ind });
+                }
+                for (int i = 1; i <= dev.joystick.Capabilities.AxesCount; i++)
+                {
+                    m.addOption("Axis " + i.ToString(), axes,
+                        new byte[] { 16, (byte)(i - 1), (byte)ind });
+                    m.addOption("IAxis " + i.ToString(), iaxes,
+                        new byte[] { 17, (byte)(i - 1), (byte)ind });
+                    m.addOption("HAxis" + i.ToString(), haxes,
+                        new byte[] { 18, (byte)(i - 1), (byte)ind });
+                    m.addOption("IHAxis" + i.ToString(), ihaxes,
+                        new byte[] { 19, (byte)(i - 1), (byte)ind });
+                }
+                m.SelectionChangeCommitted += new System.EventHandler(SelectionChanged);
+                ind++;
+            }
+        }
+
+        private string getBindingText(int i)
+        {
+            if (dev.mapping[i * 2] == 255) {
+                return "Disabled";
+            }
+            byte subType = (byte)(dev.mapping[i * 2] & 0x0F);
+            byte type = (byte)((dev.mapping[i * 2] & 0xF0) >> 4);
+            byte num = (byte)(dev.mapping[(i * 2) + 1] + 1);
+            string[] typeString = new string[] { "Button {0}", "{1}Axis {0}", "D-Pad {0} {2}" };
+            string[] axesString = new string[] { "", "I", "H", "IH" };
+            string[] dpadString = new string[] { "Up", "Down", "Left", "Right" };
+            return string.Format(typeString[type], num, axesString[subType], dpadString[subType]);
+        }
+
+        private void SelectionChanged(object sender, EventArgs e) {
+            ToolStripMenuItem i = (ToolStripMenuItem)sender;
+            byte[] b = (byte[])i.Tag;
+            if (b[0] == 254) {
+                //start thread
+                return;
+            }
+            dev.mapping[b[2] * 2] = b[0];
+            dev.mapping[(b[2] * 2) + 1] = b[1];
+            dev.Save();
+        }
+
+        private void onClose(object sender, EventArgs e) {
+            dev.Save();
+        }
+
+    }
+}
---
+++ MultiLevelComboBox.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace XOutput
+{
+    class MultiLevelComboBox : ComboBox {
+        ContextMenuStrip menu;
+
+        public new event EventHandler DropDown;
+        public new event EventHandler DropDownClosed;
+        public new event EventHandler SelectionChangeCommitted;
+
+        private bool _DroppedDown;
+
+        public MultiLevelComboBox()
+            : base()
+        {
+            base.DropDownStyle = ComboBoxStyle.DropDownList;
+            menu = new ContextMenuStrip();
+            base.Items.Add("");
+            base.SelectedItem = "";
+        }
+
+        public ToolStripMenuItem addMenu(string name, ToolStripMenuItem parent = null) {
+            ToolStripMenuItem item = new ToolStripMenuItem();
+            item.Text = name;
+            if (parent != null) {
+                parent.DropDownItems.Add(item);
+            } else {
+                menu.Items.Add(item);
+            }
+            return item;
+        }
+
+        public ToolStripMenuItem addOption(string name, ToolStripMenuItem parent = null, object tag = null) {
+            ToolStripMenuItem item = new ToolStripMenuItem();
+            item.Text = name;
+            item.Tag = tag;
+            item.Click += (sender, e) => selectValue(item);
+            if (parent != null)
+            {
+                parent.DropDownItems.Add(item);
+            }
+            else
+            {
+                menu.Items.Add(item);
+            }
+            return parent;
+        }
+
+        private void selectValue(ToolStripMenuItem m) {
+            Items[0] = m.Text;
+            DroppedDown = false;
+            if (SelectionChangeCommitted != null)
+                SelectionChangeCommitted(m, EventArgs.Empty);
+        }
+
+        private void showDropDown() {
+            menu.Show(this, new Point(0, Height));
+            menu.AutoSize = false;
+            menu.Width = Width;
+            if (DropDown != null)
+                DropDown(this, EventArgs.Empty);
+        }
+
+        private void hideDropDown() {
+            menu.Hide();
+            if (DropDownClosed != null)
+                DropDownClosed(this, EventArgs.Empty);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x87: //WM_GETDLGCODE
+                    break;//Block key down messages
+                case 0x201: //WM_LBUTTONDOWN
+                    DroppedDown = !DroppedDown;
+                    break;
+                case 0x203: //WM_LBUTTONDBLCLK
+                    DroppedDown = !DroppedDown;
+                    break;
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+        public new bool DroppedDown {
+            get { return _DroppedDown;  }
+            set { _DroppedDown = value;
+                if (value) { showDropDown(); } else { hideDropDown(); }
+            }
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        [Browsable(false), ReadOnly(true)]
+        public new ComboBoxStyle DropDownStyle
+        {
+            get { return base.DropDownStyle; }
+            set { }
+        }
+
+    }
+}
